588c73471c988886db97557287d7da5fdfd6b71b
ygae0118 <kim.yg.ae0118@gmail.com>
2022-06-18 09:47:37 +0700

    Initial commit

---
+++ lib/models/savedItem.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class SavedBook {
+  late int malId;
+  late String url;
+  late String imageUrl;
+  late String title;
+
+  SavedBook( {
+      required this.malId,
+      required this.url,
+      required this.imageUrl,
+      required this.title});
+}
+
+// if you want source code please subscribe in my channel and contact me with my emil :
+// brhomapps@gmail.com
+// good luck and thanks //
